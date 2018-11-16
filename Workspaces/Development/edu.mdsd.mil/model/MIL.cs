2638c7bd1214e102bd11cf6dd45323355bd20682
Bittner <bittner@cg.cs.tu-bs.de>
2018-11-16 14:27:38 +0100

    MIL model and grammar

---
+++ Workspaces/Development/edu.mdsd.mil/model/MIL.cs
@@ -0,0 +1,33 @@
+SYNTAXDEF mil
+FOR <http://mdsd.edu/mil/1.0>
+START MILModel
+
+OPTIONS {
+	reloadGeneratorModel = "true";
+}
+
+TOKENS {
+	DEFINE IDENTIFIER_TOKEN $('a'..'z'|'A'..'Z'|'_') ('a'..'z'|'A'..'Z'|'_'|'0'..'9')*$;
+	DEFINE INTEGER_TOKEN $('-')?('0'..'9')+$;
+	
+	DEFINE SL_COMMENT $'//'(~('\n'|'\r'|'\uffff'))*$;
+	DEFINE ML_COMMENT $'/*'.*'*/'$;
+}
+
+TOKENSTYLES {
+	"IDENTIFIER_TOKEN" COLOR #6A3E3E;
+	"INTEGER_TOKEN" COLOR #0000C0;
+	"SL_COMMENT", "ML_COMMENT" COLOR #3F7F5F;
+}
+
+RULES {
+	// syntax definition for class 'MILModel'
+	MILModel ::= instructions*;
+	
+	LoadInstruction ::= "lod" value;
+	StoreInstruction ::= "sto" registerReference?;
+	AddInstruction ::= "add";
+	
+	ConstantInteger ::= rawValue[INTEGER_TOKEN];
+	RegisterReference ::= address[IDENTIFIER_TOKEN];
+}
